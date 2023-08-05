9d0abccd44348b898c941643faf98cf4c6c9edbf
annssva <curlyannas0@gmail.com>
2023-08-05 16:43:55 +0300

    the start screen is ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import '../css/style.css'
+import {
+    darkModeHandle
+} from "./utils.js";
+
+darkModeHandle();
+
+const startGameButton = document.getElementById('startGame')
+startGameButton.addEventListener('click', () => {
+    console.log('start game');
+})
